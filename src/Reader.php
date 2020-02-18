53735a98266c87835aa0c65422eef426b331fa62
Karol Nowakowski <przeslijmi@gmail.com>
2020-02-18 21:17:21 +0100

    travis

--- src/Reader.php
+++ src/Reader.php
@@ -296,6 +296,7 @@ class Reader
 
         // Read in Sheets.
         $pattern = '/(xl)(\\\\|\\/)(worksheets)(\\\\|\\/)(sheet)(\\d)+(.xml)$/';
+        var_dump($this->allFiles);
         var_dump($pattern);
         foreach (preg_grep($pattern, $this->allFiles) as $fileUri) {
             var_dump($fileUri);
